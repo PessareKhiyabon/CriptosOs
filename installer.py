0b5e9527f52a2d30c4089652ce3191149acca30b
PessareKhiyabon <75219873+PessareKhiyabon@users.noreply.github.com>
2023-01-16 22:10:07 +0330

    Add files via upload

---
+++ installer.py
@@ -0,0 +1,26 @@
+import os
+print("Installing CriptosOS v1.1 .....")
+print("")
+try :
+    os.system("pip3 install jsonlib")
+    os.system("pip3 install tdm")
+    os.system("pip3 install colorama")
+    os.system("pip3 install base2048")
+    os.system("pip3 install kiwi-platform")
+    os.system("pip3 install pyExploitDb")
+    os.system("pip3 install nmap")
+    os.system("pip3 install sockets")
+    os.system("pip3 iinstall pip-custom-platform")
+    os.system("pip3 install scapy")
+    os.system("pip3 install db-sqlite3")
+    os.system("pip3 install pysqlite3")
+    os.system("pip3 install googlesearch-python")
+    os.system("pip3 install stepic")
+    os.system("pip3 install Pillow")
+    os.system("pip3 install Faker")
+    os.system("pip3 install pycopy-webbrowser")
+    os.system("pip3 install urllib3")
+    print("Installed !! Run ")
+    
+except :
+    print("I Can't Install .....")
